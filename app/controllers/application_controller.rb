473fdf911fd066eae7689457e80c0ae25dde09ef
Collin Gardner <collingardner5@gmail.com>
2021-09-13 21:04:48 -0600

    views started

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
 protect_from_forgery with: :null_session
 before_action :authenticate_user!
+before_action :configure_permitted_paramaters, if: :devise_controller?
+
+protected
+  # params.require(:sign_up).permit(:email, :password, :first_name, :last_name)
+  def configure_permitted_paramaters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end 
 end
