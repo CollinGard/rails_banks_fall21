1dd05982969c39fdb136495d56ddd2b4078c1ea3
Collin Gardner <collingardner5@gmail.com>
2021-09-14 19:42:47 -0600

    account js added

--- app/javascript/components/Accounts.js
+++ app/javascript/components/Accounts.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
 import Navbar from './Navbar';
-const Accounts = ({}) => {
 
-return(
-  <>
-  <Navbar />
-  <h1>Accounts</h1>
-
-</>  
-)
-
+const Accounts = ({ accounts, user }) => {
+  const { first_name, last_name } = user
+  return (
+    <>
+      <Navbar />
+      <h1>{first_name} {last_name}'s Accounts</h1>
+      <div className="collection">
+        {
+          accounts.map( (account) => (
+            <a href={`/accounts/${account.id}`} className="collection-item">
+              {account.account_name} - ${account.balance}
+            </a>
+          ))
+        }
+      </div>
+    </>
+  )
 }
+
 export default Accounts;
